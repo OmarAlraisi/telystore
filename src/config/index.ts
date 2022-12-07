263692b23a71787e29f92878cfe553b9afa946b4
AlRaisi96 <o.raisi@hotmail.com>
2022-12-07 09:36:03 +0400

    Sets up dev environment

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  API_BASE_URL: process.env.API_BASE_URL as string,
+  GET_PRODICTS_API_ENDPOINT: process.env.GET_PRODICTS_API_ENDPOINT as string,
+  AUTH_API_ENDPOINT: process.env.AUTH_API_ENDPOINT as string,
+};
