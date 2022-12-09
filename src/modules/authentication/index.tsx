d615a8a890b758aa3ac2619803ef51a04102b05f
AlRaisi96 <o.raisi@hotmail.com>
2022-12-09 22:21:55 +0400

    Completes the styling of the login page

--- src/modules/authentication/index.tsx
+++ src/modules/authentication/index.tsx
@@ -9,12 +9,34 @@ const Login = () => {
           <Image fileName="logo.png" className="logo--image" />
           <Image fileName="telystore.png" className="telystore--image" />
           <Image fileName="inventory.png" className="inventory--image" />
-          {/* <span className="login--card--text">
-            Monitor and manage <br />
-            your inventory
-          </span> */}
         </div>
-        <div className="login--card--right">Login Info</div>
+        <div className="login--card--right">
+          <span className="login--card--text">Welcome to TelyStore</span>
+          <form onSubmit={() => {}} className="login--form">
+            <div className="login--field">
+              <label className="login--input-label" htmlFor="email">
+                Email Address
+              </label>
+              <input
+                type="text"
+                name="email"
+                className="login--input-field"
+                placeholder="email@example.com"
+              />
+            </div>
+            <div className="login--field">
+              <label className="login--input-label" htmlFor="email">
+                Password
+              </label>
+              <input
+                type="password"
+                className="login--input-field"
+                placeholder="********"
+              />
+            </div>
+            <input type="submit" value="Login" className="login--form-submit" />
+          </form>
+        </div>
       </div>
     </div>
   );
