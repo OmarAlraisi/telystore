3677055f7457d3c7f1160bd1a06209002d15af51
AlRaisi96 <o.raisi@hotmail.com>
2022-12-09 22:42:16 +0400

    Uses configureStore instead of createStore

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,22 +1,7 @@
-// redux imports
-import { identity } from "lodash";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-
-// Reducers import
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
 
-// Create enhancers => To display app state on redux debugger
-let reactDevTool = identity;
-reactDevTool =
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  window.__REDUX_DEVTOOLS_EXTENSION__();
-const enhancers = [applyMiddleware(thunk), reactDevTool];
-const enhancer = compose(...enhancers);
-
-// create a store
-export const store = createStore(rootReducer, {}, enhancer as any);
+export const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
