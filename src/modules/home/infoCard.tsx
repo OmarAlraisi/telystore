e87591d990679fae6e5bd751985be8c82ddc7603
AlRaisi96 <o.raisi@hotmail.com>
2022-12-11 22:46:13 +0400

    Designs and styles the general information card

--- src/modules/home/infoCard.tsx
+++ src/modules/home/infoCard.tsx
@@ -1,14 +1,21 @@
 import "./infoCard.css";
-import classnames from "classnames";
+import Image from "../common/image";
 
-interface InfoCardProps {
-  className?: string;
-}
-
-const InfoCard = ({ className }: InfoCardProps) => {
+const InfoCard = () => {
   return (
-    <div className={classnames("info-card--root", className)}>
-      <h1>General Information</h1>
+    <div className="info-card--root">
+      <Image fileName="logo.png" className="info-card--logo" />
+      <div className="info-card--details">
+        <span className="info-card--text">{`Products: ${10}`}</span>
+        <span className="info-card--text">{`In Stock: ${220}`}</span>
+        <span className="info-card--text">{`Available: ${6} of ${10}`}</span>
+        <span className="info-card--text">{`Total Price: $${(1975).toFixed(
+          2,
+        )}`}</span>
+        <span className="info-card--text">{`Average Price: $${(
+          1975 / 220
+        ).toFixed(2)}`}</span>
+      </div>
     </div>
   );
 };
--- src/modules/home/productsGrid.tsx
+++ src/modules/home/productsGrid.tsx
@@ -13,53 +13,55 @@ const ProductsGrid = ({ className }: ProductsGridProps) => {
 
   return (
     <div className={classnames("products-grid--root", className)}>
-      {products.map((product) => {
-        return (
-          <div className="product-card">
-            {!product.isAvailable && (
-              <Image
-                fileName="unavailable.png"
-                className="unavailable--image"
-              />
-            )}
-            <span
-              className={classnames("product-card--text title", {
-                unavailable: !product.isAvailable,
-              })}
-            >
-              {product.name}
-            </span>
-            <div
-              className={classnames("product-card--details", {
-                unavailable: !product.isAvailable,
-              })}
-            >
-              <span className="product-card--text">{`In Stock: ${product.noInStock}`}</span>
-              <span className="product-card--text">{`Cost/Piece: $${product.price.toFixed(
-                2,
-              )}`}</span>
-              <span className="product-card--text">{`Total Cost: $${(
-                product.price * product.noInStock
-              ).toFixed(2)}`}</span>
-            </div>
-            <div className="product-card--id-and-controlls">
-              <div className="product-card--controlls">
-                <button className="controlls--btn delete">Delete</button>
-                <button className="controlls--btn edit">Edit</button>
-              </div>
+      <div className="products-grid--body">
+        {products.map((product) => {
+          return (
+            <div className="product-card">
+              {!product.isAvailable && (
+                <Image
+                  fileName="unavailable.png"
+                  className="unavailable--image"
+                />
+              )}
               <span
-                className={classnames("product-card--text identification", {
+                className={classnames("product-card--text title", {
                   unavailable: !product.isAvailable,
                 })}
-              >{`ID: ${product.id
-                .toPrecision(8)
-                .split(".")
-                .reverse()
-                .join("")}`}</span>
+              >
+                {product.name}
+              </span>
+              <div
+                className={classnames("product-card--details", {
+                  unavailable: !product.isAvailable,
+                })}
+              >
+                <span className="product-card--text">{`In Stock: ${product.noInStock}`}</span>
+                <span className="product-card--text">{`Cost/Piece: $${product.price.toFixed(
+                  2,
+                )}`}</span>
+                <span className="product-card--text">{`Total Cost: $${(
+                  product.price * product.noInStock
+                ).toFixed(2)}`}</span>
+              </div>
+              <div className="product-card--id-and-controlls">
+                <div className="product-card--controlls">
+                  <button className="controlls--btn delete">Delete</button>
+                  <button className="controlls--btn edit">Edit</button>
+                </div>
+                <span
+                  className={classnames("product-card--text identification", {
+                    unavailable: !product.isAvailable,
+                  })}
+                >{`ID: ${product.id
+                  .toPrecision(8)
+                  .split(".")
+                  .reverse()
+                  .join("")}`}</span>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
